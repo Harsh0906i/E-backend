75d708a678e096716efb7637bb3f3292c7bc285f
Harshit <harshitsingharya24@gmail.com>
2024-10-25 13:18:22 +0530

    updated

--- routes/Items.js
+++ routes/Items.js
@@ -207,10 +207,18 @@ router.post('/delete', verifyUser, async (req, res) => {
     }
 });
 
-router.post('/admin', async (req, res) => {
+router.post('/admin/:userId', verifyUser, async (req, res) => {
     const { productId, action } = req.body;
+    const { userId } = req.params;
+    const { id } = req.user
 
     try {
+
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({ message: 'User not found!' });
+        }
+
         const tempProduct = await TempItem.findById(productId);
 
         if (!tempProduct) {
@@ -236,7 +244,12 @@ router.post('/admin', async (req, res) => {
 
             await newItem.save();
             await TempItem.findByIdAndDelete(productId);
-            return res.status(200).json({ message: 'Product accepted and moved to Item collection!' });
+            
+            if (userId === id && user.isAdmin === 'true') {
+                return res.status(200).json({ message: 'Product accepted and moved to Item collection!', newItem });
+            } else {
+                return res.status(500).json({ message: 'Error occured while fetching the data' });
+            }
         }
 
         if (action === 'reject') {
